3f3ff2d1f094c3882a4bb2e856580e4598dcb6f8
Prateek Muley <prateek.muley@in.ey.com>
2024-07-03 07:03:30 +0000

    My stable appl

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("ats.pm.travelproces.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
